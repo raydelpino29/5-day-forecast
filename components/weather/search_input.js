1b1a09250e72cb08ddfbafad10bf0882a8f42f18
ray delpino <raydelpino29@gmail.com>
2017-12-21 17:01:49 -0500

    send request for forecast onSubmit of input

--- components/weather/search_input.js
+++ components/weather/search_input.js
@@ -1,16 +1,31 @@
 import { Component } from 'react';
+import axios from 'axios';
 
 export default class SearchInput extends Component {
   constructor(props) {
     super(props);
+    this.state = {zip: ""};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange (e) {
+    this.setState({ zip: e.target.value });
+  }
+
+  handleSubmit () {
+    axios.get(`api.openweathermap.org/data/2.5/forecast?zip=${this.state}`)
+    .then((weather) => {
+      debugger
+    });
   }
 
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <p>Type in your zip code to see how awful the weather is in your area this week!</p>
-          <input></input>
+          <input onChange={this.handleChange}></input>
         </form>
         <style jsx>{`
           input {
