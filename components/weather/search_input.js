05922a23709260279c686d468e33315789b72818
ray delpino <raydelpino29@gmail.com>
2017-12-21 16:40:00 -0500

    remove unnecessary dependencies that Next.js takes care of

--- components/weather/search_input.js
+++ components/weather/search_input.js
@@ -8,10 +8,17 @@ export default class SearchInput extends Component {
   render() {
     return (
       <div>
-        <input></input>
+        <form>
+          <p>Type in your zip code to see how awful the weather is in your area this week!</p>
+          <input></input>
+        </form>
         <style jsx>{`
           input {
             border: 1px solid purple;
+          }
+          p {
+            display: block;
+            color: black;
           }
         `}</style>
       </div>
