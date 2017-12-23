3c9d1f705857d955253038e9128b5fc078843bd2
ray delpino <raydelpino29@gmail.com>
2017-12-22 20:58:16 -0500

    parse forecast and set local state with info

--- components/weather/search_input.js
+++ components/weather/search_input.js
@@ -14,14 +14,46 @@ export default class SearchInput extends Component {
     this.setState({ zip: e.target.value });
   }
 
-  handleSubmit (e) {
+  parseForecast (weather) {
+    const date = new Date().toString().split(" "); // ex: ["Sat", "Sep", "09", "2017", "00:00:00", "GMT-0400", "(EDT)"]
+    let monthLength = 31;
+    const thirtymonths = ["Sep", "Apr", "Jun", "Nov"];
+    if (date[1] === "Feb") {
+       monthLength = parseInt(date[3]) % 4 === 0 ? 29 : 28;
+    } else if (thirtymonths.includes(date[1])) {
+      monthLength = 30;
+    }
+    let fiveDays = new Set();
+    let count = 0;
+    while (Object.values(fiveDays).length < 5) {
+      const day = parseInt(new Date().toString().split(" ")[2]);
+      fiveDays.push((day + count) % monthLength);
+      count++;
+    }
+    let forecast = {};
+    Object.values(weather.data.list).filter((info) => {
+      const date = new Date(info.dt).toString().split(" ");
+      const dayNumber = date[2];
+      const weekDay = date[0];
+
+      if (forecast[dayNumber]) {
+        continue;
+      } else if (fiveDays.has(dayNumber)) {
+        const temp = Math.floor((info.main.temp * 9/5) - 459.67);
+        forecast[weekDay] = temp;
+      }
+    });
+    this.setState({ forecast });
+  }
+
+  handleSubmit () {
     e.preventDefault();
     const baseUrl = "https://api.openweathermap.org/data/2.5/forecast?zip=";
     const zip = `${this.state.zip}`;
     const fullUrl = baseUrl + zip + "&appid=670e6d2b31b62201dc47b79f5a87b500";
     axios.get(fullUrl).then((weather) => {
       console.log(weather);
-      this.setState({ forecast: weather });
+      this.parseForecast(weather);
     });
   }
 
